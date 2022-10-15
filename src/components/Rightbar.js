8f9b68e27d1ae6096c2833150cad24169be66a40
Julia Tran <julia.kim.tran@hotmail.com>
2022-10-15 15:27:43 -0400

    Start sidebar, add avatar group

--- src/components/Rightbar.js
+++ src/components/Rightbar.js
@@ -1,10 +1,23 @@
 import React from 'react'
-import {Box} from '@mui/material'
+import { Box, Typography } from '@mui/material'
+import Avatar from '@mui/material/Avatar';
+import AvatarGroup from '@mui/material/AvatarGroup';
 
 const Rightbar = () => {
   return (
-   <Box bgcolor='red' flex={2} p={2} sx={{display:{ xs: "none", sm: "block"}}}>
-      Rightbar
+    <Box  flex={2} p={2} sx={{ display: { xs: "none", sm: "block" } }}>
+      <Box position='fixed'>
+        <Typography variant='h6' fontWeight={200}>
+          Online Friends
+        </Typography>
+        <AvatarGroup max={7}>
+      <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+      <Avatar alt="Travis Howard" src="/static/images/avatar/2.jpg" />
+      <Avatar alt="Cindy Baker" src="/static/images/avatar/3.jpg" />
+      <Avatar alt="Agnes Walker" src="/static/images/avatar/4.jpg" />
+      <Avatar alt="Trevor Henderson" src="/static/images/avatar/5.jpg" />
+    </AvatarGroup>
+      </Box>
     </Box>
   )
 }
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -22,6 +22,7 @@ const Sidebar = () => {
    };
    return (
       <Box flex={1} p={2} sx={{ display: { xs: "none", sm: "block" } }}>
+         <Box position='fixed'>
          <List>
             <ListItem disablePadding>
                <ListItemButton component='a' href='#'>
@@ -91,6 +92,7 @@ const Sidebar = () => {
                </ListItemButton>
             </ListItem>
          </List>
+         </Box>
       </Box>
    )
 }
